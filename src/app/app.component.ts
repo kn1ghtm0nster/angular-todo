c68236046517d73ec2f2785f3f4b2afb006444ed
Diego Quintanilla <diego.quintanilla@proton.me>
2023-05-08 21:21:34 -0400

    add starting work for angular todo-list app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'todo-list-app';
+  title = 'todo list';
+
+  filter: 'all' | 'active' | 'done' = 'all';
+
+  allItems = [
+    { description: 'eat', done: true },
+    { description: 'sleep', done: false },
+    { description: 'play', done: false },
+    { description: 'workout', done: false },
+  ];
+
+  get items(): any[] {
+    if (this.filter === 'all') {
+      return this.allItems;
+    }
+
+    return this.allItems.filter((item) =>
+      this.filter === 'done' ? item.done : !item.done
+    );
+  }
+
+  addItem(description: string): void{
+    this.allItems.unshift({description, done: false});
+  }
 }
